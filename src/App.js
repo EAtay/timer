7dbcd39d0ae2deed948482b0cc705346ead60aa0
EA <atai.estebesov@gmail.com>
2022-11-22 01:15:35 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import "./App.scss";
+import { useState, useEffect } from "react";
 import Timer from "./components/Timer";
 
 function App() {
+  const [timerDays, setDays] = useState();
+  const [timerHours, setHours] = useState();
+  const [timerMinutes, setMinutes] = useState();
+
+  let interval;
+
+  const startTimer = () => {
+    const dayX = new Date(2022, 10 - 1, 26, 4, 30).getTime();
+
+    interval = setInterval(() => {
+      const dataNow = new Date().getTime();
+
+      const timeDistance = dataNow - dayX;
+
+      const days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      ); 
+      const minutes = Math.floor(
+        (timeDistance % (1000 * 60 * 60)) / (1000 * 60)
+      );
+
+      if(timeDistance > 0 ){
+        setDays(days);
+        setHours(hours);
+        setMinutes(minutes);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  })
+
   return (
     <div className="App">
-      <Timer/>
+      <Timer
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+      />
     </div>
   );
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,23 +1,8 @@
 import React from "react";
 import "./Timer.scss";
 
-// const interfal = setInterval(countUp, 0);
-// function countUp() {
-const dataNow = new Date().getTime();
-const dayX = new Date(2022, 10 - 1, 26, 4, 30);
-const timeDistance = dataNow - dayX;
-let days = 0;
-const hours = Math.floor(
-  (timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-);
-const minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
-while (dayX < dataNow) {
-  dayX.setDate(dayX.getDate() + 1);
-  days++;
-}
-// }
 
-const Timer = () => {
+const Timer = ({ timerDays, timerHours, timerMinutes }) => {
   return (
     <div className="container">
       <ul className="time">
@@ -27,15 +12,15 @@ const Timer = () => {
         </li>
         <li className="block">
           <p className="block__title">Days</p>
-          <span className="days">{days}</span>
+          <span className="days">{timerDays}</span>
         </li>
         <li className="block">
           <p className="block__title">Hours</p>
-          <span className="hours">{hours}</span>
+          <span className="hours">{timerHours}</span>
         </li>
         <li className="block">
           <p className="block__title">Minutes</p>
-          <span className="minutes">{minutes}</span>
+          <span className="minutes">{timerMinutes}</span>
         </li>
       </ul>
     </div>
